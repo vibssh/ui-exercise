908efe67fd0bb3162cffd51221e1ef606dece693
Leo Jacobs <leo@the-earthworks.com>
2017-07-04 11:59:01 +0100

    * Mobile First - Styles done for Header Footer and content and aside column as per the Exercise #1

--- App/Js/test.js
+++ App/Js/test.js
@@ -5,3 +5,39 @@
  * Dependencies : jQuery 
  * Author       : Leo Jacobs
  */
+
+/* Mobile Menu */
+var _MobileMenu = (function() {
+    'use strict';
+
+    //Cache the elements
+    var settings = {
+        $menuBtn: $('.mobile-menu'),
+        $nav: $('nav')
+    };
+
+    //Event Handlers 
+    var bindUIActions = function() {
+        settings.$menuBtn.on('click', function(e) {
+            e.preventDefault();
+            settings.$nav.slideToggle();
+        });
+    };
+
+    //Public Method
+    var init = function() {
+        bindUIActions();
+    };
+
+    return {
+        init: init
+    }
+
+}());
+
+//Initializing the Mobile Menu on the 
+if ($(window).width() <= 1023) {
+    $(document).ready(function() {
+        _MobileMenu.init();
+    });
+}
